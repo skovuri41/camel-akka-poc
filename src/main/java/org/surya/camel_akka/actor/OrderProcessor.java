04869e7b88ae1a0a379d982ae69dc560324c7681
unknown <surya46584@gmail.com>
2012-02-21 18:04:53 -0500

    new actor class

---
+++ src/main/java/org/surya/camel_akka/actor/OrderProcessor.java
@@ -0,0 +1,14 @@
+package org.surya.camel_akka.actor;
+
+import akka.actor.Actor;
+import akka.actor.UntypedActor;
+
+public class OrderProcessor extends UntypedActor implements Actor {
+
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
